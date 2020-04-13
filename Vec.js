4bdf2194527e3158fd59b916027be5525cfc7d23
Elijah Cirioli <50853223+ElijahCirioli@users.noreply.github.com>
2020-04-12 18:15:29 -0700

    Minify

--- Vec.js
+++ Vec.js
@@ -1,14 +1,2 @@
-class Vec {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	equals = (v) => {
-		return this.x === v.x && this.y === v.y;
-	};
-
-	distance = (v) => {
-		return Math.sqrt((v.x - this.x) * (v.x - this.x) + (v.y - this.y) * (v.y - this.y));
-	};
-}
+class Vec{constructor(x,y){this.x=x;this.y=y}
+equals=(v)=>{return this.x===v.x&&this.y===v.y};distance=(v)=>{return Math.sqrt((v.x-this.x)*(v.x-this.x)+(v.y-this.y)*(v.y-this.y))}}
